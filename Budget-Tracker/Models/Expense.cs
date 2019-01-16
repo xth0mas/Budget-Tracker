e3d73e83caa8ef18bba76eee5e2c14b54f5a1465
xth0mas <peper2010@gmail.com>
2019-01-16 18:57:54 +0100

    Add Models directory and two model classes inside.

---
+++ Budget-Tracker/Models/Expense.cs
@@ -0,0 +1,9 @@
+﻿namespace Budget_Tracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
