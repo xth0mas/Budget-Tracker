30f266e7b69e89b5398fd6051f6d0fa4d3c407de
xth0mas <peper2010@gmail.com>
2019-01-16 20:52:36 +0100

    Divide project into layers using Clean Architecture.

---
+++ src/ApplicationCore/Entities/Expense.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Expense : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ src/ApplicationCore/Entities/Income.cs
@@ -0,0 +1,8 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Income : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
