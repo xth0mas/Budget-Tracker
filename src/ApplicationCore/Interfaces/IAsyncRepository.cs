86495b21c56086077c5551659f6ea08920eb2826
xth0mas <peper2010@gmail.com>
2019-02-07 21:41:04 +0100

    Add several interfaces to ApplicationCore

---
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
