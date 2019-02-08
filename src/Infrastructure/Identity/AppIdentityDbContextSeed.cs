82abc5a6be9067ed67c956fb48562b291dfcf5a5
xth0mas <peper2010@gmail.com>
2019-02-08 21:44:37 +0100

    Add Identity to Infrastructure

---
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser { UserName = "test@test.com", Email = "test@test.com" };
+            await userManager.CreateAsync(defaultUser, "password");
+        }
+    }
+}
